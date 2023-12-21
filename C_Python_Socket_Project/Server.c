6e847b1ad9dc453757c9ca5e98653631eeffa2b6
Adebowale-Morakinyo <adebowalemorakinyo@gmail.com>
2023-12-21 22:47:19 +0100

    Create socket/ Configure server address

--- C_Python_Socket_Project/Server.c
+++ C_Python_Socket_Project/Server.c
@@ -12,6 +12,22 @@
 void *handle_client(void *arg);
 
 int main() {
+    int server_socket, client_socket;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_len = sizeof(client_addr);
+    
+    // Create socket
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1) {
+        perror("Error creating socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // Configure server address
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
     return 0;
 }
 
