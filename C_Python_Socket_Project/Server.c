45a9eccc711b23297be7d0679db83e572f5332b7
Adebowale-Morakinyo <adebowalemorakinyo@gmail.com>
2023-12-21 22:42:24 +0100

    preprocessor libs needed

--- C_Python_Socket_Project/Server.c
+++ C_Python_Socket_Project/Server.c
@@ -1,4 +1,9 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <pthread.h>
 
 int main(char *argv[], int argc) {
